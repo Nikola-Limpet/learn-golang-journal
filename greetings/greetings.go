b6e6ef6005d7540919e539e88ef6d5c90c57ece8
ls <2023465lim@aupp.edu.kh>
2025-04-21 09:57:53 +0700

    slice, array

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // hello returns a greeting for the named person.
@@ -14,7 +15,42 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 	// nil means no error, go can return multiple values
+}
+
+func Hellos(name []string) (map[string]string, error) {
+	// If no name is given, return an error with a message.
+	if len(name) == 0 {
+		return nil, errors.New("empty name")
+	}
+
+	// Create a map to associate names with messages.
+	messages := make(map[string]string)
+
+	// Loop through the received names and call the Hello function to get the message for each name.
+	for _, n := range name {
+		message, err := Hello(n)
+		if err != nil {
+			return nil, err
+		}
+		messages[n] = message
+	}
+	return messages, nil
+}
+
+// randomFormat returns one of a set of greeting messages.
+
+func randomFormat() string {
+	// A slice of message formats.
+
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying a random index.
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -10,13 +10,11 @@ import (
 func main() {
 	// Set properties of the predefined variables
 	// the log entry prefix and a flag to diable printing
-	// the date and time
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
 	// Request a greeting message
-
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Nick")
 
 	// if an error was returned, print it to the console and exit
 	if err != nil {
@@ -26,4 +24,47 @@ func main() {
 	// if no error was returned, print the message
 	// to the console
 	fmt.Println(message)
+
+	// Array and Slice examples
+
+	// Array use fix length
+	array := [2]string{"Peeny", "Penny"}
+	fmt.Println(array[0])
+	fmt.Println(array[1])
+
+	// Slice use dynamic length
+	slice := []string{"Peeny", "Penny"}
+	fmt.Println(slice[0])
+	fmt.Println(slice[1])
+
+	s := make([]byte, 5, 6) // when the length is 5, the capacity is 6. when it is inuse it will be 5
+	fmt.Println(s)
+
+	// s == []byte{0, 0, 0, 0, 0}
+	fmt.Println(len(s)) // 5
+	fmt.Println(cap(s)) // 6
+
+	// slicing of slice and array
+
+	x := []byte{'a', 'b', 'c', 'd', 'e'}
+
+	// x[1:4] == []byte{'b', 'c', 'd'}, shearing the storage as x
+	fmt.Println(x[0:2]) // [a b]
+	fmt.Println(x[1:3]) // [b c]
+	fmt.Println(x[2:4]) // [c d]
+
+	y := [3]string{"sdf", "sd", "sdf"}
+	z := y[:]      // [sdf sd sdf]
+	fmt.Println(z) // [sdf sd sdf]
+	// slice is a reference type, so it will share the same storage as the array. it is different from the array
+	// that it is not completely copied. it is just a reference to the array
+	// for example it does not copy the array, it just points to the same memory location
+
+	d := []byte{'r', 'o', 'a', 'd'}
+	e := d[2:] // [a d]
+	// e == []byte{'a', 'd'}
+
+	e[1] = 'm'
+	// e == []byte{'a', 'm'}
+	// d == []byte{'r', 'o', 'a', 'm'}  // d is changed because e is a reference to d
 }
